c7ae79f93b94b3b8cd138989f16f8f4bf116f533
Ashwani0044 <ashwanib707@gmail.com>
2026-08-24 23:45:34 +0530

    link update

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = https://bytechain.onrender.com;
+const API_BASE_URL = "https://bytechain.onrender.com";
 
 async function request(path, options = {}) {
   const response = await fetch(`${API_BASE_URL}${path}`, {
